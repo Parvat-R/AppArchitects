3d901ce13fd2d4bf80611d945c7b3f92550c43ae
mano <mrheadset172715@gmail.com>
2024-05-24 13:11:21 +0530

    Create of LoginUI.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,3 @@
-val <T> Provider<T>.android: T
-    get() {
-        TODO("Not yet implemented")
-    }
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -56,7 +51,6 @@ android {
 
 dependencies {
 
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -65,7 +59,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
