3ad5584be35d83edb16421a30d4c1c3932501775
mano <mrheadset172715@gmail.com>
2024-05-24 12:05:26 +0530

    Create of LoginUI.kt

--- app/src/main/java/com/example/aavinapp/LoginUI.kt
+++ app/src/main/java/com/example/aavinapp/LoginUI.kt
@@ -1,2 +1,46 @@
 package com.example.aavinapp
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+//@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginUI(sessionDetails: SessionDetails) {
+    Scaffold (
+        modifier = Modifier
+    ){
+        innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+            ) {
+            Column (modifier = Modifier
+                .padding(top = 200.dp)
+                .padding(horizontal = 120.dp)){
+
+                Text(text = "USER LOGIN", style = TextStyle(fontSize = 30.sp))
+//                var farmerID by remember { mutableStateOf("")}
+//                TextField(
+//                    value = farmerID,
+//                    onValueChange = { farmerID = it },
+//                    label = { Text("Farmer ID") }
+//                )
+//                var password by remember { mutableStateOf("")}
+//                TextField(
+//                    value = farmerID,
+//                    onValueChange = { password = it },
+//                    label = { Text("password") }
+//                )
+
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/aavinapp/SessionDetails.kt
@@ -0,0 +1,6 @@
+package com.example.aavinapp
+
+data class SessionDetails(
+    var username: String,
+    var sessionId: String
+)
