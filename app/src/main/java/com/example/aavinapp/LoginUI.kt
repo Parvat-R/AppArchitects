351308c6e0f647d401f639c97d915fdf982a7d94
mano <mrheadset172715@gmail.com>
2024-05-24 13:01:15 +0530

    Create of LoginUI.kt

--- app/src/main/java/com/example/aavinapp/LoginUI.kt
+++ app/src/main/java/com/example/aavinapp/LoginUI.kt
@@ -4,45 +4,55 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
-
-
 //@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginUI(sessionDetails: SessionDetails) {
-    Scaffold (
-        modifier = Modifier
-    ){
-        innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding),
-            ) {
-            Column (modifier = Modifier
-                .padding(top = 200.dp)
-                .padding(horizontal = 120.dp)){
-
-                Text(text = "USER LOGIN", style = TextStyle(fontSize = 30.sp))
-//                var farmerID by remember { mutableStateOf("")}
-//                TextField(
-//                    value = farmerID,
-//                    onValueChange = { farmerID = it },
-//                    label = { Text("Farmer ID") }
-//                )
-//                var password by remember { mutableStateOf("")}
-//                TextField(
-//                    value = farmerID,
-//                    onValueChange = { password = it },
-//                    label = { Text("password") }
-//                )
-
-            }
-        }
+//    Scaffold(
+//        modifier = Modifier
+//    ) { innerPadding ->
+//        Column(
+//            modifier = Modifier.padding(innerPadding),
+//        ) {
+//            Column(
+//                modifier = Modifier
+//                    .padding(top = 200.dp)
+//                    .padding(horizontal = 120.dp)
+//            ) {
+//
+//
+//            }
+//        }
+//    }
+    Column (modifier = Modifier
+                    .padding(top = 200.dp)
+                    .padding(horizontal = 120.dp)){
+        Text(text = "USER LOGIN")
+        var farmerID by remember { mutableStateOf("") }
+        TextField(
+            value = farmerID,
+            onValueChange = { farmerID = it },
+            label = { Text(text = "Farmer ID") }
+        )
+        var password by remember { mutableStateOf("") }
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text(text = "password") }
+        )
     }
-
 }
+
+
+
